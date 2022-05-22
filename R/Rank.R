478c884a1cbb6fa84af560f1c3f86d40e11a1f23
Arnaud <arnaud.malapert@univ-cotedazur.fr>
2022-05-22 23:08:56 +0200

    add parameter 'decreasing'  to function Rank

--- R/Rank.R
+++ R/Rank.R
@@ -1,17 +1,19 @@
 #' Returns the sample fractional ranks of the values in a vector, list or data frame.
 #'
+#' Rank the argument in increasing order.
 #' Missing values (‘NA’s) are put last, but their ranks are not averaged.
 #'
 #' @param x a vector, list, or data frame.
 #'
 #' @return A numeric vector of the same length as ‘x’ without names.
 #' The vector is of double type because the ranking use the average method (whether or not there are any ties).
-#'
-#' @seealso https://stackoverflow.com/a/27036534
+#' @section References:
+#' https://en.wikipedia.org/wiki/Ranking
+#' https://stackoverflow.com/a/27036534
 #'
 #' @export
 #' @examples
-#' df <- data.frame(x = c(NA, 1, 2), y = c(12:1, NA, NA, NA))
+#' df <- data.frame(x = c(1, 2, NA), y = c(12:1, NA, NA, NA))
 #' df$r <- Rank(df)
 #' df[ order(df$r), ]
 #'
@@ -19,11 +21,18 @@
 #' df$r <- NULL
 #' df$r <- Rank(df)
 #' df[ order(df$r), ]
-Rank<- function(x) {
+Rank<- function(x, decreasing = FALSE) {
     if(is.list(x)) {
-        rank <- order(do.call("order", c(x, na.last = TRUE)))
-        rank <-  do.call("ave", list(x=rank, x))
-        return(rank)
-    } else if(is.vector(x)) return(rank(unname(x), ties.method = "average", na.last = TRUE))
-    return(NULL)
+        ranks <- order(do.call("order", c(x, na.last = TRUE)))
+        ranks <-  do.call("ave", list(x=ranks, x))
+    } else if(is.vector(x) || is.factor(x)) {
+        ranks <- rank(unname(x), ties.method = "average", na.last = TRUE)
+    } else {
+        warning("Returning NULL - cannot rank ", toString(x))
+        return(NULL)
+    }
+    if(decreasing) {
+        ranks <- length(ranks) + 1 - ranks
+    }
+    return(ranks)
 }
--- tests/testthat/testRank.R
+++ tests/testthat/testRank.R
@@ -1,23 +1,41 @@
 test_that("Rank vector", {
     n <- 50
     x <- seq(n)
+    y <- rev(x)
     expect_equal(x, Rank(x))
-    x <- rev(x)
-    expect_equal(x, Rank(x))
+    expect_equal(y, Rank(x, decreasing = TRUE))
+
+    expect_equal(y, Rank(y))
+    expect_equal(x, Rank(y, decreasing = TRUE))
 })
 
 test_that("Rank list and data frame", {
     n <- 50
     m <- 2
     x <- rep(seq(m), each = n)
+    rx <- rev(x)
     y <- seq( m * n)
+    ry <- rev(y)
     expect_equal(y, Rank(list(x, y)))
+    expect_equal(ry, Rank(list(x, y), decreasing = TRUE))
     expect_equal(y, Rank(data.frame(x, y, y)))
+    expect_equal(ry, Rank(data.frame(x, y, y), decreasing = TRUE))
 
-    x <- rev(x)
-    y <- rev(y)
-    expect_equal(y, Rank(list(x, y)))
-    expect_equal(y, Rank(data.frame(x, y, y)))
+    expect_equal(ry, Rank(list(rx, ry)))
+    expect_equal(y, Rank(list(rx, ry), decreasing = TRUE))
+    expect_equal(ry, Rank(data.frame(rx, ry, ry)))
+    expect_equal(y, Rank(data.frame(rx, ry, ry), decreasing = TRUE))
+})
+
+test_that("Rank with missing values", {
+
+    df <- data.frame(x = c(1, 2, NA), y = c(12:1, NA, NA, NA))
+    expect_equal(c(4, 9, 14, 3, 8, 13, 2, 7, 12, 1, 6, 11, 5, 10, 15), Rank(df))
+
+    df <- rbind(df, df)
+    expect_equal(c(7.5, 17.5, 27, 5.5, 15.5, 25, 3.5, 13.5, 23, 1.5, 11.5, 21,
+                   9, 19, 29, 7.5, 17.5, 28, 5.5, 15.5, 26, 3.5, 13.5, 24, 1.5,
+                   11.5, 22, 10, 20, 30), Rank(df))
 })
 
 test_that("Rank Random data frames", {
@@ -34,5 +52,6 @@ test_that("Rank Random data frames", {
         ## map vector order onto integers
         x <- n * (n * df$x + df$y) + df$z
         expect_equal(Rank(x), Rank(df))
+        expect_equal(Rank(-x), Rank(df, decreasing = TRUE))
     }
 })
